c3b5afaebba2217cde11f2813e922b84eaa5fb8b
RobertoDebarba <roberto.debarba@gmail.com>
2014-03-10 13:04:38 -0300

    Adicionada classe de recebimento em cliente

--- OpenLync/src/TrataCliente.java
+++ OpenLync/src/TrataCliente.java
@@ -11,54 +11,54 @@ public class TrataCliente implements Runnable {
 	   }
 	 
 	   public void run() {
-	     // quando chegar uma msg, distribui pra todos
-		 
-		   // Inicia Imput Strim
-	     Scanner s = null;
-	     	// Instancia imout strem
-		try {
-			s = new Scanner(this.cliente.getInputStream());
-		} catch (IOException e) {
-		}
-		
-	     while (s.hasNextLine()) {
-	       //servidor.distribuiMensagem(s.nextLine());
-	    	 
-	    	 // Aqui começa as pira
-	    	 // Verificar o IP do cliente e mardar ------------------------------------------------
-	    	 String remetente = cliente.getInetAddress().getHostAddress(); // Salva ip do servidor em var remetente
-	    	 
-	    	 // Varre linha para capturar ip de destino ---
-	    	 
-	    	 boolean pParte = true;
-	    	 String ipDestino = "";
-	    	 String mensagem = "";
-	    	 
-	    	 for(int i=0;i<(s.nextLine()).length();i++){  
-	    		   char c = (s.nextLine()).charAt(i);
-	    		   
-	    		   if (c == '|') {
-	    			   pParte = false;
-	    		   }
-	    		   else if (pParte) {
-	    			   ipDestino = ipDestino + c;
-	    		   }
-	    		   else if (!pParte) {
-	    			   mensagem = mensagem + c;
-	    		   }
-	    	 }  
-	    	 
-	    	 String msg = remetente + "|" + mensagem;
-	    	 
-	    	 // Envia mensagem (seja o que Deus quiser) ---
-	    	 
+			// quando chegar uma msg, distribui pra todos
+			 
+			// Inicia Imput Strim
+			Scanner s = null;
+			// Instancia imout strem
 			try {
-				OpenLync.mandaMensagem(ipDestino, msg);
+				s = new Scanner(this.cliente.getInputStream());
 			} catch (IOException e) {
 			}
-
-	    	 //System.out.print(s.nextLine());
-	     }
-	     s.close();
+			
+			 while (s.hasNextLine()) {
+				 //servidor.distribuiMensagem(s.nextLine());
+				 
+				 // Aqui começa as pira
+				 // Verificar o IP do cliente e mardar ------------------------------------------------
+				 String remetente = cliente.getInetAddress().getHostAddress(); // Salva ip do servidor em var remetente
+				 
+				 // Varre linha para capturar ip de destino ---
+				 
+				 boolean pParte = true;
+				 String ipDestino = "";
+				 String mensagem = "";
+				 
+				 for(int i=0;i<(s.nextLine()).length();i++){  
+					   char c = (s.nextLine()).charAt(i);
+					   
+					   if (c == '|') {
+						   pParte = false;
+					   }
+					   else if (pParte) {
+						   ipDestino = ipDestino + c;
+					   }
+					   else if (!pParte) {
+						   mensagem = mensagem + c;
+					   }
+				 }  
+				 
+				 String msg = remetente + "|" + mensagem;
+				 
+				 // Envia mensagem (seja o que Deus quiser) ---
+				 
+				try {
+					OpenLync.mandaMensagem(ipDestino, msg);
+				} catch (IOException e) {
+				}
+				
+				 //System.out.print(s.nextLine());
+			 }
+			 s.close();
 	   }
 }
