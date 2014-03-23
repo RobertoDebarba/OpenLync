694d7fc23453bfae756711ee7dc647c5f16c4582
RobertoDebarba <roberto.debarba@gmail.com>
2014-03-23 01:34:41 -0300

    Adicionada base de dados

---
+++ OpenLync/base.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE OpenLync;
+USE OpenLync;
+
+CREATE TABLE usuarios (
+	codigo_usuario INT(3) PRIMARY KEY AUTO_INCREMENT,
+	nome_usuario VARCHAR(45) NOT NULL,
+	login_usuario VARCHAR(10) NOT NULL,
+	senha_usuario VARCHAR(10) NOT NULL,
+	status_usuario boolean,
+	ip_usuario VARCHAR(15)
+);
+
+INSERT INTO usuarios(nome_usuario, login_usuario, senha_usuario, status_usuario) VALUES ('Roberto Luiz Debarba', 'roberto', '123', false);
+INSERT INTO usuarios(nome_usuario, login_usuario, senha_usuario, status_usuario) VALUES ('Maria da Silva', 'maria', '123', false);
+INSERT INTO usuarios(nome_usuario, login_usuario, senha_usuario, status_usuario) VALUES ('Joao Felipe', 'joao', '123', false);
