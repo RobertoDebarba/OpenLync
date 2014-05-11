4ea41886b9179eeb8a9d450993bd572c50f22710
robertodebarba <roberto.debarba@gmail.com>
2014-05-11 00:18:36 -0300

    Ajuste Base tb_mensagens: Adicionado campo lido_mensagens em tb_mensagens; Insert em tb_mensagens e tb_amigos;

--- OpenLync/DataBase/base.sql
+++ OpenLync/DataBase/base.sql
@@ -35,7 +35,8 @@ CREATE TABLE tb_amigos (
 CREATE TABLE tb_mensagens (
 	codigo_mensagem INT(3) AUTO_INCREMENT,
 	conteudo_mensagem VARCHAR(150) NOT NULL,
-	data_mensagem VARCHAR(14) NOT NULL,
+	data_mensagem DATETIME,
+	lido_mensagem boolean NOT NULL,
 	codigo_remet_mensagem INT(3),
 	codigo_dest_mensagem INT(3),
 
@@ -48,6 +49,8 @@ CREATE TABLE tb_mensagens (
 	PRIMARY KEY (codigo_mensagem)
 );
 
+/* Cargos */
+
 INSERT INTO tb_cargos(desc_cargo)
 VALUES ('Programador de Sistemas');
 
@@ -57,6 +60,8 @@ VALUES ('Analista de Sistemas');
 INSERT INTO tb_cargos(desc_cargo)
 VALUES ('Diretor de Desenvolvimento');
 
+/* Usuarios */
+
 INSERT INTO tb_usuarios(nome_usuario, login_usuario, senha_usuario, ip_usuario, codigo_cargo, foto_usuario)
 VALUES ('Roberto Luiz Debarba', '35685347493069331629356853630934749'/*roberto*/, '154051571716029'/*123*/, 'null', 1, LOAD_FILE('/home/roberto/roberto.jpg'));
 
@@ -65,3 +70,31 @@ VALUES ('Maria da Silva', '3412530381356853287730381'/*maria*/, '154051571716029
 
 INSERT INTO tb_usuarios(nome_usuario, login_usuario, senha_usuario, ip_usuario, codigo_cargo, foto_usuario)
 VALUES ('João Felipe Figueiro', '33189347493038134749'/*joao*/, '154051571716029'/*123*/, 'null', 3, LOAD_FILE('/home/roberto/joao.jpg'));
+
+/* Amigos */
+
+INSERT INTO tb_amigos(codigo_usuario_amigo, codigo_amigo_amigo)
+VALUES (1, 2);
+
+INSERT INTO tb_amigos(codigo_usuario_amigo, codigo_amigo_amigo)
+VALUES (1, 3);
+
+INSERT INTO tb_amigos(codigo_usuario_amigo, codigo_amigo_amigo)
+VALUES (2, 1);
+
+INSERT INTO tb_amigos(codigo_usuario_amigo, codigo_amigo_amigo)
+VALUES (3, 1);
+
+/* Mensagens */
+
+INSERT INTO tb_mensagens(conteudo_mensagem, data_mensagem, lido_mensagem, codigo_remet_mensagem, codigo_dest_mensagem)
+VALUES ('Mensagem de teste0001. Hello World!', '2014-04-08 12:24:36', true, 1, 2);
+
+INSERT INTO tb_mensagens(conteudo_mensagem, data_mensagem, lido_mensagem, codigo_remet_mensagem, codigo_dest_mensagem)
+VALUES ('Mensagem de teste0002. Hello World!', '2014-04-08 12:24:36', true, 1, 2);
+
+INSERT INTO tb_mensagens(conteudo_mensagem, data_mensagem, lido_mensagem, codigo_remet_mensagem, codigo_dest_mensagem)
+VALUES ('Mensagem de teste0003. Hello World!', '2014-04-08 12:24:36', true, 1, 3);
+
+INSERT INTO tb_mensagens(conteudo_mensagem, data_mensagem, lido_mensagem, codigo_remet_mensagem, codigo_dest_mensagem)
+VALUES ('Mensagem de teste0004. Hello World!', '2014-04-08 12:24:36', true, 2, 1);
