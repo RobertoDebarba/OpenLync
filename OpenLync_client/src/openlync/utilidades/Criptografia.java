a87fc7bbc3a35207f262d24a7d8b82f24c978422
robertodebarba <roberto.debarba@gmail.com>
2014-06-08 15:14:24 -0300

    Ajuste de Criptogafia

--- OpenLync_client/src/openlync/utilidades/Criptografia.java
+++ OpenLync_client/src/openlync/utilidades/Criptografia.java
@@ -1,8 +1,11 @@
 package openlync.utilidades;
+
+import javax.swing.JOptionPane;
+
 public class Criptografia {
 
 	/**
-	 * Retorna mensagem criptografada
+	 * @return mensagem criptografada
 	 */
 	public String criptografarMensagem(String mensagem) {
 
@@ -14,6 +17,13 @@ public class Criptografia {
 			int letraCript = (((x * 8) + 3) * 39); // Realiza calculo de
 													// criptografia
 			String letraCriptStr = ("" + letraCript); // Converte int para STR
+			
+			//Valida tamanho do codigo gerado
+			if ((letraCriptStr.length() != 5) && ((!letraCriptStr.equals("2925")) && (!letraCriptStr.equals("3237")))) {
+				letraCriptStr = "";
+				JOptionPane.showMessageDialog(null, "O caracter "+c+" não é suportado!", "Erro de criptografia", 1);
+			}
+			
 			mensagemCript = mensagemCript + letraCriptStr;
 		}
 
@@ -21,32 +31,45 @@ public class Criptografia {
 	}
 
 	/**
-	 * Retorna mensagem descriptografada
+	 * @return mensagem descriptografada
 	 */
 	public String descriptografarMensagem(String mensagem) {
 
 		String mensagemDescript = "";
 
-		for (int z = 0; z < mensagem.length(); z++) { // Varre da linha
-			String carac = "";
-
-			int contador = (z + 5); // Seta tamanho do codigo asc a ser
-									// descript.
-			for (int i = z; i < contador; i++) { // Captura numero asc a ser
-													// calculado
-				carac = (carac + mensagem.charAt(i));
-				if (i != (contador - 1)) { // Se não for ultimo numero do codigo
-					z++; // Incrementa contador geral de caracter na linha
+		try {
+			for (int z = 0; z < mensagem.length(); z++) { // Varre da linha
+				String carac = "";
+	
+				int contador = (z + 5); // Seta tamanho do codigo asc a ser
+										// descript.
+				for (int i = z; i < contador; i++) { // Captura numero asc a ser
+														// calculado
+					carac = (carac + mensagem.charAt(i));
+					
+					// -- RESTRI��ES
+					if (carac.equals("2925")) { 		//Se for TAB
+						i++;
+					} else if (carac.equals("3237")) { 	//Se for ENTER
+						i++;
+					}
+					
+					if (i != (contador - 1)) { // Se n�o for ultimo numero do codigo
+						z++; // Incrementa contador geral de caracter na linha
+					}
 				}
+	
+				int asc = (((Integer.parseInt(carac) / 39) - 3) / 8); // Calculo
+																		// inverso
+																		// de
+																		// descriptografia
+				char x = (char) asc; // Converte asc para letra
+	
+				mensagemDescript = mensagemDescript + x;
 			}
-
-			int asc = (((Integer.parseInt(carac) / 39) - 3) / 8); // Calculo
-																	// inverso
-																	// de
-																	// descriptografia
-			char x = (char) asc; // Converte asc para letra
-
-			mensagemDescript = mensagemDescript + x;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Ocorreu um erro ao descriptografar uma mensagem!", "Erro de Criptografia", 0);
+			return null;
 		}
 
 		return mensagemDescript;
